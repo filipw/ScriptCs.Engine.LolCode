d62d14a5e576ed2cfc3a4ad73b999ce108e09c44
filipw <wojcieszyn@gmail.com>
2014-07-20 11:59:52 +0200

    first version

---
+++ ScriptCs.Engine.LolCode/LolCodeEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using notdot.LOLCode;
+using ScriptCs.Contracts;
+
+namespace ScriptCs.Engine.LolCode
+{
+    public class LolCodeEngine : IScriptEngine
+    {
+        public ScriptResult Execute(string code, string[] scriptArgs, AssemblyReferences references, IEnumerable<string> namespaces,
+            ScriptPackSession scriptPackSession)
+        {
+            var debugLine = code.Substring(0, code.IndexOf(".lol\"") + 5);
+            code = code.Replace(debugLine, string.Empty).TrimStart();
+
+            var compiler = new LOLCodeCodeProvider();
+            var cparam = new CompilerParameters
+            {
+                GenerateInMemory = true,
+                MainClass = "Program",
+                OutputAssembly = "lolcode.dll"
+            };
+            cparam.ReferencedAssemblies.AddRange(references.PathReferences != null ? references.PathReferences.ToArray() : new string[0]);
+            var results = compiler.CompileAssemblyFromSource(cparam, code);
+
+            var x = results.CompiledAssembly.GetReferencedAssemblies();
+
+            var startupType = results.CompiledAssembly.GetType("Program", true, true);
+            var instance = Activator.CreateInstance(startupType, false);
+            var entryPoint = results.CompiledAssembly.EntryPoint;
+
+            var result = entryPoint.Invoke(instance, new object[] { });
+
+            return new ScriptResult(result);
+        }
+
+        public string BaseDirectory { get; set; }
+        public string CacheDirectory { get; set; }
+        public string FileName { get; set; }
+    }
+}
---
+++ ScriptCs.Engine.LolCode/LolCodeModule.cs
@@ -0,0 +1,13 @@
+﻿using ScriptCs.Contracts;
+
+namespace ScriptCs.Engine.LolCode
+{
+    [Module("lolcode")]
+    public class LolCodeModule : IModule
+    {
+        public void Initialize(IModuleConfiguration config)
+        {
+            config.ScriptEngine<LolCodeEngine>();
+        }
+    }
+}
